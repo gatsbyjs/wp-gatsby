e7b1bf9f778f14cf61a59bf7afd34550a7c5b1c8
Tyler Barnes <tylerdbarnes@gmail.com>
2020-11-04 16:04:18 -0800

    simplify start function invocation since the logic was moved into the start fn

--- src/Admin/assets/src/start-preview-client.js
+++ src/Admin/assets/src/start-preview-client.js
@@ -5,22 +5,18 @@ import { fetchPreviewStatusAndUpdateUI } from "./preview-status"
  * This file is printed out in preview-template.php
  * initialState global comes from preview-template.php above where this is printed to the page
  */
-;(async () => {
-	try {
-		await start()
-	} catch (e) {
-		console.error(e)
-		if (document.readyState === "complete" || document.readyState === "loaded") {
-			// document is already ready to go so show the error
+start().catch((e) => {
+	console.error(e)
+	if (document.readyState === "complete" || document.readyState === "loaded") {
+		// document is already ready to go so show the error
+		showError(e)
+	} else {
+		// otherwise wait for it to load before showing the error
+		document.addEventListener("DOMContentLoaded", () => {
 			showError(e)
-		} else {
-			// otherwise wait for it to load before showing the error
-			document.addEventListener("DOMContentLoaded", () => {
-				showError(e)
-			})
-		}
+		})
 	}
-})()
+})
 
 async function start() {
 	const [, fetchResponse] = await Promise.all([
