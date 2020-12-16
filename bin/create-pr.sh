8705f14acba597f8d5fe78d621712b328fc9c330
Rikki <rikki.schulte@gmail.com>
2020-12-16 14:32:50 -0500

    ci: use ocotokit/request-action instead of curl

--- bin/create-pr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/gatbsyjs/gatsby-source-wordpress-experimental/actions/workflows/wp-gatsby-pr.yml/dispatches \
-  -d '{"ref":"chore/test-$GITHUB_REF", "inputs": { "version": $GITHUB_REF }}'
