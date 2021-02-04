9fa301aea1cbf6ac3cd68b4f5592cae2a36eb4ae
Tyler Barnes <tylerdbarnes@gmail.com>
2021-02-03 18:35:07 -0800

    Revert "update composer autoload"
    This reverts commit 33bd0d495acd80f10c452963be1a8af838158cf5.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,11 +13,16 @@ class ComposerAutoloaderInitd5ba67e9c3910011804380f1fed1cebe
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
+
+        require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitd5ba67e9c3910011804380f1fed1cebe', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
@@ -25,7 +30,7 @@ class ComposerAutoloaderInitd5ba67e9c3910011804380f1fed1cebe
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitd5ba67e9c3910011804380f1fed1cebe::getInitializer($loader));
         } else {
@@ -47,24 +52,6 @@ class ComposerAutoloaderInitd5ba67e9c3910011804380f1fed1cebe
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitd5ba67e9c3910011804380f1fed1cebe::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequired5ba67e9c3910011804380f1fed1cebe($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequired5ba67e9c3910011804380f1fed1cebe($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
