e2f5363fc62bbffb917bc7903db299664dd53e58
Tyler Barnes <tylerdbarnes@gmail.com>
2021-07-06 09:43:55 -0700

    v1.0.11

--- wp-gatsby.php
+++ wp-gatsby.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Gatsby
  * Description: Optimize your WordPress site to be a source for Gatsby sites.
- * Version: 1.0.10
+ * Version: 1.0.11
  * Author: GatsbyJS, Jason Bahl, Tyler Barnes
  * Author URI: https://gatsbyjs.org
  * Text Domain: wp-gatsby
@@ -103,7 +103,7 @@ final class WPGatsby
     {
         // Plugin version.
         if (! defined('WPGATSBY_VERSION') ) {
-            define('WPGATSBY_VERSION', '1.0.10');
+            define('WPGATSBY_VERSION', '1.0.11');
         }
 
         // Plugin Folder Path.
