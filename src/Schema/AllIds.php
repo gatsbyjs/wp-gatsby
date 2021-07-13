0a7c5bfa42607efeeccad1def5a24f1085642f1c
Tyler Barnes <tylerdbarnes@gmail.com>
2021-07-13 13:15:40 -0700

    Update AllIds.php

--- src/Schema/AllIds.php
+++ src/Schema/AllIds.php
@@ -2,40 +2,21 @@
 
 namespace WPGatsby\AllIds;
 
-use GraphQLRelay\Relay;
-
 class AllIds {
     function __construct() {
         add_action('graphql_register_types', function() {
 
-			register_graphql_object_type('WPGatsby', [
-				'description' => __( 'WP-Gatsby related fields', 'WPGatsby' ),
-				'fields' => [
-					'isWPGatsby' => [
-						'type' => 'Boolean',
-						'description' => '',
-						'resolve' => function() { return true; },
-					],
-				]
-			]);
-
             register_graphql_object_type('WpGatsbyIdAndType', [
 				'description' => __( 'WP-Gatsby related fields', 'WPGatsby' ),
 				'fields' => [
-					'id' => [
-						'type' => 'ID',
+					'ids' => [
+						'type' => [ 'list_of' => 'Int' ],
                     ],
                     'type' => [
                         'type' => 'String'
                     ]
 				]
 			]);
-
-			register_graphql_field('RootQuery', 'wpGatsby', [
-				'description' => '',
-				'type' => 'WPGatsby',
-				'resolve' => function() { return true; },
-            ]);      
 
             register_graphql_field(
                 'WPGatsby',
@@ -44,149 +25,84 @@ class AllIds {
                     'type' => ['list_of' => 'WpGatsbyIdAndType'],
                     'description' => '',
                     'resolve' => function() {
-                        $nodes = [];
+                        $node_ids_by_type = [];
 
+                        // 
+                        // Get all taxonomy term id's
                         $taxonomies = get_taxonomies( [ 'show_in_graphql' => true ], OBJECT );
 
                         if ( is_array( $taxonomies ) && ! empty( $taxonomies ) ) {
                             foreach ( $taxonomies as $taxonomy ) {
-                                $nodes = array_merge(
-                                    $nodes,
-                                    [[
-                                        'id' => base64_encode( 'taxonomy:' . $taxonomy->name ),
-                                        'type' => 'Taxonomy'
-                                    ]],
-                                );
+                                $term_ids = get_terms( $taxonomy->name, [ 'hide_empty' => false, 'fields' => 'ids' ] );
+                                $taxonomy_type_name = ucfirst( $taxonomy->graphql_single_name );
 
-                                $terms = get_terms( $taxonomy->name, [ 'hide_empty' => false, 'fields' => 'ids' ] );
-
-                                $terms_normalized = array_map(
-                                    function ( $term_id ) use ($taxonomy) {
-                                        return [
-                                            'id' => base64_encode( 'term:' . $term_id ),
-                                            'type' => ucfirst( $taxonomy->graphql_single_name )
-                                        ];
-                                    },
-                                    $terms
-                                );
-
-                                $nodes = array_merge(
-                                    $nodes,
-                                    $terms_normalized
-                                );
+                                if (array_key_exists($taxonomy_type_name, $node_ids_by_type)) {
+                                    $node_ids_by_type[$taxonomy_type_name] = array_merge($node_ids_by_type[$taxonomy_type_name], $term_ids);
+                                } else {
+                                    $node_ids_by_type[$taxonomy_type_name] = $term_ids;
+                                }
                             }
                         }
+                        // 
 
-                        $menu_ids = get_terms( [
-                            'taxonomy' => 'nav_menu',
-                            'hide_empty' => false,
+
+                        // // 
+                        // // Get all Menu id's
+                        // $menu_ids = get_terms( [
+                        //     'taxonomy' => 'nav_menu',
+                        //     'hide_empty' => false,
+                        //     'fields' => 'ids'
+                        // ] );
+
+                        // $node_ids_by_type['Menu'] = $menu_ids;
+                        // // 
+
+                        // // 
+                        // // Get all Menu item id's
+                        // $menu_items_query = new \WP_Query([
+                        //     'post_type' => 'nav_menu_item',
+                        //     'posts_per_page' => -1,
+                        //     'fields' => 'ids',
+                        //     'no_found_rows' => true,
+                        // ]);
+
+                        // $menu_item_ids = $menu_items_query->posts;
+
+                        // $node_ids_by_type['MenuItem'] = $menu_item_ids;
+                        // // 
+
+                        // 
+                        // Get all Comment id's
+                        $comment_ids = get_comments( [
+                            'hierarchical' => 'flat',
+                            'post_type' => 'any',
                             'fields' => 'ids'
                         ] );
 
-                        $normalized_menus = array_map(
-                            function($menu_id) {
-                                return [
-                                    'id' => base64_encode( 'term:' . $menu_id ),
-                                    'type' => 'Menu'
-                                ];
-                            },
-                            $menu_ids
-                        );
+                        $node_ids_by_type['Comment'] = $comment_ids;
+                        // 
 
-                        $nodes = array_merge(
-                            $nodes,
-                            $normalized_menus
-                        );
 
-                        $menu_items_query = new \WP_Query([
-                            'post_type' => 'nav_menu_item',
-                            'posts_per_page' => -1,
-                            'fields' => 'ids',
-                            'no_found_rows' => true,
-                        ]);
-
-                        if ( !empty( $menu_items_query->posts ) ) {
-                            $formatted_menu_items = array_map(
-                                function($post_id) {
-                                    return [ 
-                                        'id' => Relay::toGlobalId(
-                                            'nav_menu_item',
-                                            absint( $post_id )
-                                        ),
-                                        'type' => 'MenuItem',
-                                    ];
-                                },
-                                $menu_items_query->posts
-                            );
-
-                            $nodes = array_merge(  $nodes, $formatted_menu_items );   
-                        }
-
-                        $comments = get_comments( [
-                            'hierarchical' => 'flat',
-                            'post_type' => 'any'
-                        ] );
-
-                        if ( !empty( $comments ) ) {
-                            $formatted_comments = array_map(
-                                function($comment) {
-                                    error_log(print_r($comment, true)); 
-                                    return [ 
-                                        'id' => Relay::toGlobalId(
-                                            'comment',
-                                            absint( $comment->comment_ID )
-                                        ),
-                                        'type' => 'Comment',
-                                    ];
-                                },
-                                $comments
-                            );
-
-                            $nodes = array_merge(  $nodes, $formatted_comments );   
-                        }
-
-                        $users = get_users( [
+                        // 
+                        // Get all User id's
+                        $user_ids = get_users( [
                             'fields' => 'ids',
                             'has_published_posts' => true,
                             'number' => -1
                         ] );
 
-                        $users_normalized = array_map(
-                            function($user_id) {
-                                return [
-                                    'id' => Relay::toGlobalId(
-                                        'user',
-                                        absint( $user_id )
-                                    ),
-                                    'type' => 'User',
-                                ];
-                            },
-                            $users
-                        );
+                        $node_ids_by_type['User'] = $user_ids;
+                        // 
 
-                        $nodes = array_merge(
-                            $nodes,
-                            $users_normalized,
-                        );
-
+                        // 
+                        // Get all post id's
                         $post_types = get_post_types([ 'show_in_graphql' => true ], 'objects');
 
 
                         foreach($post_types as $post_type) {
-                            if ($post_type->name === 'action_monitor') {
+                            if ($post_type->name === 'action_monitor' || $post_type->name === 'attachment') {
                                 continue;
                             }
-
-                            array_push(
-                                $nodes,
-                                [
-                                    'id' => Relay::toGlobalId(
-                                        'post_type',
-                                        $post_type->name,
-                                    ),
-                                    'type' => 'ContentType'
-                                ]
-                            );
 
                             $query = new \WP_Query([
                                 'post_type' => $post_type->name,
@@ -196,27 +112,23 @@ class AllIds {
                                 'no_found_rows' => true
                             ]);
     
-                            if ( !empty( $query->posts ) ) {
-                                $formatted_posts = array_map(
-                                    function($post_id) use($post_type) {
-                                        return [ 
-                                            'id' => Relay::toGlobalId(
-                                                'post',
-                                                absint( $post_id )
-                                            ),
-                                            'type' => ucfirst(
-                                                $post_type->graphql_single_name 
-                                            ),
-                                        ];
-                                    },
-                                    $query->posts
-                                );
+                            $post_type_typename = ucfirst(
+                                $post_type->graphql_single_name 
+                            );
 
-                                $nodes = array_merge(  $nodes, $formatted_posts );   
-                            }
+                            $post_type_ids = $query->posts;
+                            $node_ids_by_type[$post_type_typename] = $post_type_ids;
                         }
+                        // 
 
-                        return $nodes;
+                        $format_response = function($typename, $ids) {
+                            return [
+                                'type' => $typename,
+                                'ids' => $ids
+                            ];
+                        };
+
+                        return array_map($format_response, array_keys($node_ids_by_type), $node_ids_by_type);
                     }
                 ]
             );
